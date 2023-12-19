01352193004baa460b70329ce4348703beacea08
Rachwan <rachwan.harb2023@gmail.com>
2023-12-19 18:23:00 +0200

    Just add it padding/margin

--- tripoli/src/pages/TestPage/test.jsx
+++ tripoli/src/pages/TestPage/test.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import './test.css'
 
 export const Test = () => {
   return(
-    <div>
+    <div className="main">
       <h1>HElloooooo</h1>
       <p className="" style={{}}></p>
     </div>
