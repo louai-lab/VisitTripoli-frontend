b2abe0dc2707e20f0bb90e4d4636e37f57fc6d0c
louai <louaibaghdadi27@gmail.com>
2023-12-20 16:56:35 +0200

    sign up and sign in

--- tripoli/src/pages/SignIn/SignIn.jsx
+++ tripoli/src/pages/SignIn/SignIn.jsx
@@ -1,100 +1,171 @@
-import {React, useState} from "react";
-import style from './SignIn.module.css';
+import { React, useState } from "react";
+import style from "./SignIn.module.css";
+// import style from '../SignUp/SignUp.module.css'
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import googleIcon from '../../images/google.png';
-import eye from '../../images/eye.png'
-import hide from '../../images/hide.png'
+import googleIcon from "../../images/google.png";
+import eye from "../../images/eye.png";
+import hide from "../../images/hide.png";
+import { toast, ToastContainer } from "react-toastify";
 
 function SignIn() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [imageFile, setImageFile] = useState(null);
   const [showPassword, setShowPassword] = useState(false);
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
+  // const [formData, setFormData] = useState({
+  //   name: '',
+  //   email: '',
+  //   password: '',
+  // });
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
   });
 
-  const handleimageChange = (e) => {
-    setImageFile(e.target.files[0]);
-  };
-
   const visiblePassword = () => {
-    setShowPassword(!showPassword)
+    setShowPassword(!showPassword);
   };
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
 
-      setFormData({
-        ...formData,
-        [name]: value,
-      });
+    setCredentials({
+      ...credentials,
+      [name]: value,
+    });
+  };
+
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+
+  //   const formDataToSend = new FormData();
+  //   formDataToSend.append('name', formData.name);
+  //   formDataToSend.append('email', formData.email);
+  //   formDataToSend.append('password', formData.password);
+
+  //   if (imageFile) {
+  //     formDataToSend.append('image', imageFile);
+  //   }
+
+  //   try {
+  //     const response = await axios.post('http://localhost:4000/user/register', formDataToSend, {
+  //       headers: {
+  //         'Content-Type': 'multipart/form-data',
+  //       },
+  //     });
+  //     console.log('Response:', response.data);
+  //     return navigate('/', { replace: true })
+  //   } catch (error) {
+  //     console.error('Error:', error);
+  //   }
+  // };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (credentials.email === "" || credentials.password === "") {
+      return toast.error("All fields are required");
     }
 
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-    
-      const formDataToSend = new FormData();
-      formDataToSend.append('name', formData.name);
-      formDataToSend.append('email', formData.email);
-      formDataToSend.append('password', formData.password);
-    
-      if (imageFile) {
-        formDataToSend.append('image', imageFile);
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/login",
+        credentials
+      );
+
+      if (response.data) {
+        console.log(response.data);
+
+        // Save user data to local storage
+        localStorage.setItem("userData", JSON.stringify(true));
+        toast.success("Login successfully")
+        setTimeout(() => {
+          navigate("/", { replace: true });
+        }, 5000);
       }
-    
-      try {
-        const response = await axios.post('http://localhost:4000/user/register', formDataToSend, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        });
-        console.log('Response:', response.data);
-        return navigate('/', { replace: true })
-        return 
-      } catch (error) {
-        console.error('Error:', error);
+      
+    } catch (error) {
+      if (error.response.status === 401) {
+        toast.error("Incorrect email or password");
+      } else {
+        toast.error("An error occurred. Please try again.");
       }
-    };
+      console.log(error);
+    }
+  };
 
   return (
-    <main className={style.main}>
-      <div className={style.imageContainer}></div>
-      <div className={style.content}>
-        <div className={style.text}>
-          <h1 className={style.mainTitle}>Sign in to Exclusive</h1>
-          <p className={style.slogan}>Enter your details below</p>
-        </div>
-        <form onSubmit={handleSubmit} encType="multipart/form-data">
-          <div className={style.input}>
-            <input type="email" name="email" value={formData.email} onChange={handleInputChange} placeholder="Email *" required/>
+    <>
+      <ToastContainer />
+      <main className={style.main}>
+        <div className={style.imageContainer}></div>
+
+        <div style={{ margin: "50px auto", display: "flex" }}>
+          <div className={style.content}>
+            <div className={style.text}>
+              <h1 className={style.mainTitle}>Sign in to Exclusive</h1>
+              <p className={style.slogan}>Enter your details below</p>
+            </div>
+
+            <form onSubmit={handleSubmit} encType="multipart/form-data">
+              <div className={style.input}>
+                <input
+                  type="email"
+                  name="email"
+                  value={credentials.email}
+                  onChange={handleInputChange}
+                  placeholder="Email *"
+                  required
+                />
+              </div>
+
+              <div className={`${style.input} ${style.passwordInput}`}>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  onChange={handleInputChange}
+                  value={credentials.password}
+                  placeholder="Password *"
+                  required
+                />
+
+                <img
+                  src={showPassword ? hide : eye}
+                  className={style.icon}
+                  alt="Hide and Show an Eye"
+                  onClick={visiblePassword}
+                />
+              </div>
+
+              <Link to={"/"}>
+                <input
+                  type="submit"
+                  onClick={handleSubmit}
+                  value="Sign In"
+                  className={style.submitBtn}
+                />
+              </Link>
+            </form>
+
+            <div className={style.googleButton}>
+              <img
+                src={googleIcon}
+                alt="Google Icon"
+                className={style.googleIcon}
+              />
+              <p className={style.slogan}>Sign in with Google</p>
+            </div>
+            <p className={style.loginP}>
+              Don't have an account?
+              <span className={style.login}>
+                <Link to={"/signup"}>Sign up</Link>
+              </span>
+            </p>
           </div>
-          <div className={`${style.input} ${style.passwordInput}`}>
-            <input type={showPassword ? "text" : "password"} name="password" onChange={handleInputChange} value={formData.password} placeholder="Password *" required/>
-            <img
-              src={showPassword ? hide : eye}
-              className={style.icon}
-              alt="Hide and Show an Eye"
-              onClick={visiblePassword}
-            />
-          </div>
-          <Link to={"/"}>
-            <input type="submit" onClick={handleSubmit} value="Sign In" className={style.submitBtn} />
-          </Link>
-        </form>
-        <div className={style.googleButton}>
-          <img src={googleIcon} alt="Google Icon" className={style.googleIcon}/>
-          <p className={style.slogan}>Sign in with Google</p>
         </div>
-        <p className={style.loginP}>
-          Don't have an account?
-          <span className={style.login}><Link to={"/signin"}>Sign up</Link></span>
-        </p>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 
--- tripoli/src/pages/SignUp/SignUp.jsx
+++ tripoli/src/pages/SignUp/SignUp.jsx
@@ -1,20 +1,23 @@
-import {React, useState} from "react";
-import style from './SignUp.module.css';
+import { React, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import style from "./SignUp.module.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import googleIcon from '../../images/google.png';
-import eye from '../../images/eye.png'
-import hide from '../../images/hide.png'
+import googleIcon from "../../images/google.png";
+import eye from "../../images/eye.png";
+import hide from "../../images/hide.png";
+
+import "react-toastify/dist/ReactToastify.css";
 
 function SignUp() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [imageFile, setImageFile] = useState(null);
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
+    name: "",
+    email: "",
+    password: "",
   });
 
   const handleimageChange = (e) => {
@@ -22,93 +25,150 @@ function SignUp() {
   };
 
   const visiblePassword = () => {
-    setShowPassword(!showPassword)
+    setShowPassword(!showPassword);
   };
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
 
-      setFormData({
-        ...formData,
-        [name]: value,
-      });
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Validate the form fields
+  if (!formData.name || !formData.email || !formData.password) {
+    toast.error("Please fill in all required fields.");
+    return;
+  }
+
+    const formDataToSend = new FormData();
+    formDataToSend.append("name", formData.name);
+    formDataToSend.append("email", formData.email);
+    formDataToSend.append("password", formData.password);
+
+    if (imageFile) {
+      formDataToSend.append("image", imageFile);
     }
 
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-    
-      const formDataToSend = new FormData();
-      formDataToSend.append('name', formData.name);
-      formDataToSend.append('email', formData.email);
-      formDataToSend.append('password', formData.password);
-    
-      if (imageFile) {
-        formDataToSend.append('image', imageFile);
-      }
-    
-      try {
-        const response = await axios.post('http://localhost:4000/user/register', formDataToSend, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        });
-        console.log('Response:', response.data);
-        return navigate('/', { replace: true })
-        return 
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
+    axios
+      .post("http://localhost:4000/user/register", formDataToSend, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        toast.success("Signup successful!");
+
+        
+        setTimeout(() => {
+          navigate("/", { replace: true });
+        }, 5000);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        toast.error("Signup failed. Please try again.");
+      });
+  };
 
   return (
-    <main className={style.main}>
-      <div className={style.imageContainer}></div>
-      <div className={style.content}>
-        <div className={style.text}>
-          <h1 className={style.mainTitle}>Create an account</h1>
-          <p className={style.slogan}>Enter your details below</p>
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+      />
+
+      <main className={style.main}>
+        <div className={style.imageContainer}></div>
+
+        <div style={{margin:"50px auto" , display:"flex"}}>
+          <div className={style.content}>
+            <div className={style.text}>
+              <h1 className={style.mainTitle}>Create an account</h1>
+              <p className={style.slogan}>Enter your details below</p>
+            </div>
+            <form onSubmit={handleSubmit} encType="multipart/form-data">
+              <div className={style.input}>
+                <input
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  placeholder="Name *"
+                  required
+                />
+              </div>
+              <div className={style.input}>
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  placeholder="Email *"
+                  required
+                />
+              </div>
+              <div className={`${style.input} ${style.passwordInput}`}>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  onChange={handleInputChange}
+                  value={formData.password}
+                  placeholder="Password *"
+                  required
+                />
+                <img
+                  src={showPassword ? hide : eye}
+                  className={style.icon}
+                  alt="Hide and Show an Eye"
+                  onClick={visiblePassword}
+                />
+              </div>
+              <div className={`${style.input} ${style.picture}`}>
+                <label htmlFor="imageUpload" className={style.imageLabel}>
+                  Insert Your image
+                </label>
+                <input
+                  type="file"
+                  id="imageUpload"
+                  name="image"
+                  value={formData.image}
+                  className={style.uploadImageInput}
+                  onChange={handleimageChange}
+                />
+              </div>
+              <Link to={"/"}>
+                <input
+                  type="submit"
+                  onClick={handleSubmit}
+                  value="Create Account"
+                  className={style.submitBtn}
+                />
+              </Link>
+            </form>
+            <div className={style.googleButton}>
+              <img
+                src={googleIcon}
+                alt="Google Icon"
+                className={style.googleIcon}
+              />
+              <p className={style.slogan}>Sign up with Google</p>
+            </div>
+            <p className={style.loginP}>
+              Already have account?
+              <span className={style.login}>
+                <Link to={"/signin"}>LogIn</Link>
+              </span>
+            </p>
+          </div>
         </div>
-        <form onSubmit={handleSubmit} encType="multipart/form-data">
-          <div className={style.input}>
-            <input type="text" name="name" value={formData.name} onChange={handleInputChange} placeholder="Name *" required/>
-          </div>
-          <div className={style.input}>
-            <input type="email" name="email" value={formData.email} onChange={handleInputChange} placeholder="Email *" required/>
-          </div>
-          <div className={`${style.input} ${style.passwordInput}`}>
-            <input type={showPassword ? "text" : "password"} name="password" onChange={handleInputChange} value={formData.password} placeholder="Password *" required/>
-            <img
-              src={showPassword ? hide : eye}
-              className={style.icon}
-              alt="Hide and Show an Eye"
-              onClick={visiblePassword}
-            />
-          </div>
-          <div className={`${style.input} ${style.picture}`}>
-          <label htmlFor="imageUpload" className={style.imageLabel}>Insert Your image</label>
-          <input
-            type="file"
-            id="imageUpload"
-            name="image" 
-            value={formData.image}
-            className={style.uploadImageInput}
-            onChange={handleimageChange}
-          />
-          </div>
-          <Link to={"/"}>
-            <input type="submit" onClick={handleSubmit} value="Create Account" className={style.submitBtn} />
-          </Link>
-        </form>
-        <div className={style.googleButton}>
-          <img src={googleIcon} alt="Google Icon" className={style.googleIcon}/>
-          <p className={style.slogan}>Sign up with Google</p>
-        </div>
-        <p className={style.loginP}>
-          Already have account? 
-          <span className={style.login}><Link to={"/signin"}>LogIn</Link></span>
-        </p>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 
